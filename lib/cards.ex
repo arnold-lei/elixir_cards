6c7c50970d7f5004febea4235d51a665169c575b
Arnold <arnold.lei88@gmail.com>
2016-12-26 18:09:00 -0500

    adding a shuffle method

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,5 +1,8 @@
 defmodule Cards do
-    def hello do
-        "Hello World!"
+    def create_deck do
+        ["Ace", "One", "Two", "Three" ]
+    end
+    def shuffle(deck) do
+        Enum.shuffle(deck)
     end
 end
