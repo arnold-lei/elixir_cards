1ecbb7d68633d04d927df01c5b8921b66bebc626
Arnold <arnold.lei88@gmail.com>
2016-12-26 21:52:45 -0500

    Created a loop to create the deck of cards

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,8 +1,19 @@
 defmodule Cards do
     def create_deck do
-        ["Ace", "One", "Two", "Three" ]
+        values  = ["Ace", "Two", "Three", "Four"]
+        suits   = ["Diamonds", "Clubs", "Hearts", "Spades"]
+        for value <- values do
+            for suit <- suits do
+                "#{value} of #{suit}"
+            end
+        end
     end
+
     def shuffle(deck) do
         Enum.shuffle(deck)
+    end
+
+    def contains?(deck, card) do
+        Enum.member?(deck, card)
     end
 end
