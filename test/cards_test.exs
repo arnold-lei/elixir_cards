f9844c7e7f99a49c56ce2108df68b6da1179987d
Arnold Lei <arnold.lei88@gmail.com>
2016-12-29 15:31:01 -0500

    created a test for shuffling deck

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -1,8 +1,15 @@
 defmodule CardsTest do
-  use ExUnit.Case
-  doctest Cards
+    use ExUnit.Case
+    doctest Cards
 
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
+    test "create_deck makes 52 cards" do
+        deck_length = length(Cards.create_deck)
+        assert deck_length == 52
+    end
+
+    test "shuffling a deck radomizes the deck" do
+        deck = Cards.create_deck
+        refute deck == Cards.shuffle(deck)
+
+    end
 end
